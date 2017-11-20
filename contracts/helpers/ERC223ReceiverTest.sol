ece8488cd6ad8de5bde3f8b1ea07cf99628a8051
Aliaksei Hiatsevich <aliaksei.hiatsevich@instinctools.ru>
2017-11-20 18:14:39 +0300

    Test ++

---
+++ contracts/helpers/ERC223ReceiverTest.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.9;
+
+import "../core/platform/ChronoBankAssetWithCallbackListener.sol";
+
+contract ERC223ReceiverTest {
+  function tokenFallback(address _from, uint _value, bytes _data) {
+
+  }
+}
